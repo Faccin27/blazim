3755aa56149f713b6a7ced545d12d1bdfa2afbee
Guilherme Faccin <gfaccin27@gmail.com>
2025-04-10 14:02:27 -0300

    feat: created footer component

--- resources/views/site/includes/footer.blade.php
+++ resources/views/site/includes/footer.blade.php
@@ -1,69 +1,79 @@
 <footer class="footer flex_c">
-    <div class="infoFooter container">
-        <div class="main flex_w flex_r middle">
-            <div class="enderecoFooter fullW flex middle">
-                <figure class="logoF">
-                    <a href="{{ route('home') }}" class="whFitImg flex">
-                        <img src="https://dummyimage.com/180x65/" alt="{{ config('app.name') }}" title="{{ config('app.name') }}">
-                    </a>
-                </figure>
 
-                <div class="flex_c">
-                    <p>{!! nl2br($site->endereco) !!}</p>
-                    <a href="mailto:{{ $site->email }}" class="whiteFont margin10">{{ $site->email }}</a>
-                    <a href="javascript:;" class="whiteFont bold margin10" onclick="funcoesHelper.sendWhatsapp('{{ $site->whatsapp }}', 'Olá, tudo bem? Estava navegando no site {{ config('app.name') }} e gostaria de mais informações.')">
-                        {{ $site->whatsapp}}
-                    </a>
-                    <span class="whiteFont">Atendimento: 8h às 12h - 13h30 às 18h.</span>
-                </div>
-            </div>
-            <div class="sitemapFooter fullW grid4">
-                <div class="flex_c gap30">
-                    <a href="javascript:;" class="whiteFont medium font15">Notícias</a>
-                    <a href="javascript:;" target="_blank" class="whiteFont medium font15">Agenda</a>
-                    <a href="javascript:;" class="whiteFont medium font15">Editais</a>
-                    <a href="javascript:;" class="whiteFont medium font15">Serviços</a>
-                    <a href="javascript:;" class="whiteFont medium font15">Parceiros</a>
-                    <a href="javascript:;" class="whiteFont medium font15">Política de Privacidade</a>
-                </div>
+    <div class="logo-container">
+        <img src="{{ Vite::asset('resources/assets/site/img/logoblazim.png') }}" alt="Logo" class="logo" />
+        <img src="{{ Vite::asset('resources/assets/site/img/icon_face.png') }}"alt="Ícone 1" class="icon" />
+        <img src="{{ Vite::asset('resources/assets/site/img/icon_insta.png') }}" alt="Ícone 2" class="icon" />
+    </div>
 
-                <div class="flex_c">
-                    <p class="font15 whiteFont margin20">Fundação CETEPI</p>
-                    <p class="font15 whiteFont black">Polo Inovale</p>
-
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Primeiros Passos</a>
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Eixos de Desenvolvimento</a>
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Programas Estruturantes</a>
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Modelo de Gestão e Governo</a>
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Atores</a>
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Associe-se</a>
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Downloads</a>
-                </div>
-
-                <div class="flex_c footerCentro">
-                    <p class="font15 whiteFont black">Centro de Inovação</p>
-
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Quem Somos</a>
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Como Funciona</a>
-                </div>
-
-                <div class="flex_c footerHabitats">
-                    <p class="font15 whiteFont black">Habitats de Inovação</p>
-
-                    <a href="javascript:;" class="blackFont font15 medium margin15">Lorem ipsum</a>
-
-                    <li><a href="javascript:;" class="blackFont font15 medium margin15">Mapa da Inovação</a></li>
-                </div>
-            </div>
+    <div class="main margin80 flex_r">
+        <div class="coluna">
+            <p class="titulo">INSTITUCIONAL</p>
+            <ul>
+                <li>Início</li>
+                <li>Sobre</li>
+                <li>Representantes</li>
+                <li>Contato</li>
+                <li>Fale Conosco</li>
+            </ul>
         </div>
+
+        <div class="coluna">
+            <p class="titulo">MARCAS</p>
+            <ul>
+                <li>ANASOL</li>
+                <li>HIDRABENE</li>
+                <li>LOKENZZI</li>
+                <li>GREEN</li>
+                <li>POWER DENT</li>
+                <li>VEDIS</li>
+                <li>ESCOBEL</li>
+                <li>PLÁSTIBABY</li>
+                <li>GUM</li>
+                <li>FIORUCCI</li>
+                <li>GOTA DOURADA</li>
+                <li>BABYGO</li>
+                <li>DERMACHEM</li>
+                <li>INFINITO LOOKS HAIR</li>
+            </ul>
+        </div>
+
+        <div class="coluna">
+            <p class="titulo">INFORMAÇÕES E CONTATO</p>
+            <ul>
+                <li class="endereco">
+                    Rua Jabob Jacomel, 111<br>
+                    Bairro Nossa Senhora de Fátima<br>
+                    Herval d’Oeste, 89610-000
+                </li>
+                <li class="email">blazim@blazim.com.br</li>
+            </ul>
+            <div class="botao-container">
+                <button class="fale-conosco-botao">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="23" height="22" viewBox="0 0 23 22"
+                        fill="none">
+                        <path
+                            d="M0.726339 22L2.21354 16.5352C1.23359 14.8553 0.718744 12.9448 0.721939 11C0.721939 4.9247 5.64664 0 11.7219 0C17.7972 0 22.7219 4.9247 22.7219 11C22.7219 17.0753 17.7972 22 11.7219 22C9.77799 22.0031 7.86826 21.4887 6.18893 20.5095L0.726339 22ZM7.75203 5.8388C7.60998 5.84762 7.47118 5.88503 7.34393 5.9488C7.22461 6.01637 7.11568 6.10084 7.02053 6.1996C6.88853 6.3239 6.81373 6.4317 6.73343 6.5362C6.32689 7.06529 6.10821 7.71476 6.11194 8.38199C6.11414 8.92099 6.25494 9.44569 6.47493 9.93629C6.92483 10.9285 7.66513 11.979 8.64303 12.9525C8.87843 13.1868 9.10833 13.4222 9.35583 13.6411C10.5696 14.7097 12.0159 15.4803 13.5798 15.8917L14.2057 15.9874C14.4092 15.9984 14.6127 15.983 14.8173 15.9731C15.1377 15.9566 15.4505 15.8698 15.7336 15.719C15.8777 15.6448 16.0183 15.564 16.1549 15.477C16.1549 15.477 16.2022 15.4462 16.2924 15.378C16.4409 15.268 16.5322 15.1899 16.6554 15.0612C16.7467 14.9666 16.8259 14.8555 16.8864 14.729C16.9722 14.5497 17.058 14.2076 17.0932 13.9227C17.1196 13.7049 17.1119 13.5861 17.1086 13.5124C17.1042 13.3947 17.0063 13.2726 16.8996 13.2209L16.2594 12.9338C16.2594 12.9338 15.3024 12.5169 14.7183 12.2507C14.6567 12.2238 14.5907 12.2086 14.5236 12.2056C14.4484 12.1979 14.3723 12.2063 14.3006 12.2304C14.2289 12.2545 14.1632 12.2937 14.1078 12.3453V12.3431C14.1023 12.3431 14.0286 12.4058 13.2333 13.3694C13.1877 13.4307 13.1248 13.4771 13.0527 13.5026C12.9806 13.528 12.9026 13.5314 12.8285 13.5124C12.7569 13.4932 12.6866 13.469 12.6184 13.4398C12.482 13.3826 12.4347 13.3606 12.3412 13.3199L12.3357 13.3177C11.7064 13.0429 11.1238 12.6718 10.6087 12.2177C10.4701 12.0967 10.3414 11.9647 10.2094 11.8371C9.77667 11.4227 9.39952 10.9538 9.08743 10.4423L9.02253 10.3378C8.97592 10.2676 8.93823 10.1918 8.91033 10.1123C8.86853 9.95059 8.97743 9.82079 8.97743 9.82079C8.97743 9.82079 9.24473 9.52819 9.36903 9.36979C9.47253 9.23815 9.5691 9.1012 9.65833 8.95949C9.78813 8.7505 9.82883 8.53599 9.76063 8.36989C9.45263 7.6175 9.13363 6.8684 8.80583 6.1248C8.74093 5.9774 8.54843 5.8718 8.37353 5.8509C8.31413 5.8443 8.25473 5.8377 8.19533 5.8333C8.04761 5.82596 7.89958 5.82743 7.75203 5.8377V5.8388Z"
+                            fill="white" />
+                    </svg>
+                    <span class="fale-texto">Fale Conosco</span>
+                </button>
+            </div>
+
+        </div>
+
     </div>
 
     <div class="copyright">
         <div class="main flex_w flex_r middle">
             <p>Todos os direitos reservados - <a href="admin" target="_blank">Acesso Restrito</a> - <a
-                    href="https://webmail.lovatel.com.br" target="_blank">Webmail</a> - Feito por <a href="https://lovatel.com.br" target="_blank">Lovatel</a></p>
+                    href="https://webmail.lovatel.com.br" target="_blank">Webmail</a> - Feito por <a
+                    href="https://lovatel.com.br" target="_blank">Lovatel</a></p>
         </div>
     </div>
+
+
+
 </footer>
 
 
@@ -78,12 +88,13 @@
 </a>
 {{-- WhatsApp flutuante --}}
 
-<a href="javascript:;" class="whatsFlutuante flex middle" onclick="funcoesHelper.sendWhatsapp('{{ $site->whatsapp }}', 'Olá, tudo bem? Estava navegando no site {{ config('app.name') }} e gostaria de mais informações.')">
+<a href="javascript:;" class="whatsFlutuante flex middle"
+    onclick="funcoesHelper.sendWhatsapp('{{ $site->whatsapp }}', 'Olá, tudo bem? Estava navegando no site {{ config('app.name') }} e gostaria de mais informações.')">
     <span class="showWhats">Podemos Ajudar?</span>
 
     <div class="iconWhatsFlutuante showWhats pseudoElemento">
         <figure class="whFitImg">
-            <img src="{{@Vite::asset('resources/assets/site/img/wppFlutuante.svg')}}" alt="WhatsApp Icon">
+            <img src="{{ @Vite::asset('resources/assets/site/img/wppFlutuante.svg') }}" alt="WhatsApp Icon">
         </figure>
     </div>
 </a>
